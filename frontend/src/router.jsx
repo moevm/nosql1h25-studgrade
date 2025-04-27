a22a295a5a0c87fdfdb2f16446c78db7b407db57
Iwan Ryžij <teum3048@yandex.ru>
2025-04-27 21:34:12 +0300

    Update router

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,12 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 
 // Import the pages
-
 import ProfilePage from "./pages/ProfilePage";
+
+// Import the layouts
+import PageLayout from "./components/PageLayout";
 
 export const router = createBrowserRouter([
   {
-    element: <ProfilePage />,
-    path: "/profile",
+    element: <PageLayout />,
+    children: [
+      {
+        element: <ProfilePage />,
+        path: "/profile",
+      },
+      {
+        element: <div>Stats Page</div>,
+        path: "/",
+      },
+    ],
   },
 ]);
