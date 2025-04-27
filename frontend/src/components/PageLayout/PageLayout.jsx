b516cfb99cbb749cae2bb19ccb149707f6de83f4
Iwan Ryžij <teum3048@yandex.ru>
2025-04-27 20:56:26 +0300

    Add page layout

---
+++ frontend/src/components/PageLayout/PageLayout.jsx
@@ -0,0 +1,29 @@
+import { Outlet, NavLink } from "react-router-dom";
+import styles from "./PageLayout.module.css";
+import ProfileIcon from "../../public/profile.svg?react";
+import StatsIcon from "../../public/stats.svg?react";
+
+const PageLayout = () => {
+  return (
+    <div className={styles.page}>
+      <header className={styles.header}>
+        <div className={styles.name}>ФИО</div>
+      </header>
+      <div className={styles.container}>
+        <aside className={styles.aside_menu}>
+          <NavLink to={"/profile"} className={styles.aside_menu__link}>
+            <ProfileIcon /> Профиль
+          </NavLink>
+          <NavLink to={"/"} className={styles.aside_menu__link}>
+            <StatsIcon /> Статистика
+          </NavLink>
+        </aside>
+        <div className={styles.main}>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PageLayout;
