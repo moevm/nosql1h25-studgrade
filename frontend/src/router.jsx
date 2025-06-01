0ae68054778e9e619d98d9a9ca7182df1cea6ee5
IvanMavlikaev <112758292+IvanMavlikaev@users.noreply.github.com>
2025-06-01 23:40:15 +0300

    update path

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -28,7 +28,7 @@ export const router = createBrowserRouter([
       },
       {
         element: <UpdateUserPage />,
-        path: "/users/:userId",
+        path: "/users/:userId/update",
       },
     ],
   },
